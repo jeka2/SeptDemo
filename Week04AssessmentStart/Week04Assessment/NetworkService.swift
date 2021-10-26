db7f0d791c0dd6938653f20c84909d2b68857894
Luat <luat@ravebizz.com>
2021-10-26 09:14:55 -0400

    adding assessment starter

---
+++ Week04AssessmentStart/Week04Assessment/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkService.swift
+//  Week04Assessment
+//
+//  Created by Luat on 10/25/21.
+//
+
+import Foundation
+
+struct NetworkService {
+    let url = URL(string: "https://api.tvmaze.com/singlesearch/shows?q=game%20of%20thrones&embed=episodes")
+    
+}
---
+++ Week04AssessmentStart/Week04Assessment/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Week04Assessment
+//
+//  Created by Luat on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
